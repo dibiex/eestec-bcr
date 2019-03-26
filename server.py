fb073c61152605b9b6eed33d1a2a5fdaf13bdc7b
Maxim Baz <claudiu.dima@stud.acs.upb.ro>
2019-03-26 12:27:38 +0200

    push1

--- server.py
+++ server.py
@@ -2,6 +2,11 @@ from flask import Flask, request, redirect, url_for, render_template, jsonify
 app = Flask(__name__)
 
 
+a = ["Andrei", "Ioana", "Adelin"]
+
 @app.route('/', methods=['GET'])
 def get():
-    return '''hello'''
+    return render_template("tables.html", users=a)
+@app.route('/login', methods=['GET'])
+def login():
+	return render_template("login.html")
